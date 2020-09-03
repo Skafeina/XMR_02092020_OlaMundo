8b1d9e43b95a262c709917f9bf9c850329bf2db4
Skafeina <skafeina@gmail.com>
2020-09-02 22:29:37 -0300

    Modifica layout e programa botão olá mundo.

--- XMR02092020_OlaMundo/MainActivity.cs
+++ XMR02092020_OlaMundo/MainActivity.cs
@@ -9,13 +9,29 @@ namespace XMR02092020_OlaMundo
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView txtOla;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
+            
+            //Define qual layout esta classe (MainActivity) vai controlar
             SetContentView(Resource.Layout.activity_main);
+
+            //Capturando os objetos do layout para serem manipulados aqui
+            txtOla = FindViewById<TextView>(Resource.Id.TxtOla);
+            Button btnMostrar = FindViewById<Button>(Resource.Id.BtnMostrar);
+
+            //Criar o método de click do botão BtnMostrar
+            btnMostrar.Click += BtnMostrar_Click;
+
         }
+
+        private void BtnMostrar_Click(object sender, System.EventArgs e)
+        {
+            txtOla.Text = "Olá Mundo!";
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
